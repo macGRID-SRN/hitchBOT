1ee2e04949b11905df0c9ff4e6f18581ef617ec9
gagichce <gagichce@mcmaster.ca>
2014-05-09 00:03:26 -0400

    the actual initial checkin of hitchbotAPI

---
+++ server/hitchbotAPI/hitchbotAPI/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+namespace hitchbotAPI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<hitchbotAPI.Models.Database>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Models.Database context)
+        {
+            
+        }
+    }
+}
---
+++ server/hitchbotAPI/hitchbotAPI/Models/Database.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace hitchbotAPI.Models
+{
+    public class Database : DbContext
+    {
+
+        public Database()
+            : base("name=Database")
+        {
+        }
+
+        public DbSet<SpeechEvent> SpeechEvents { get; set; }
+        public DbSet<hitchBOT> hitchBOTs { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
