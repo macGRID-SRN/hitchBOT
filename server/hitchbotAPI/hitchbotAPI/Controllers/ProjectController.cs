3a653b4b22961e4be132749d7bf7899e8160595f
gagichce <gagichce@mcmaster.ca>
2014-05-09 11:13:30 -0400

    added project controller, able to create new projects

---
+++ server/hitchbotAPI/hitchbotAPI/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using hitchbotAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace hitchbotAPI.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        [HttpPost]
+        public bool StartProject(string Name, string Description)
+        {
+            using (var db = new Database())
+            {
+                var newProject = new Project();
+                newProject.Name = Name;
+                newProject.Description = Description;
+                newProject.StartTime = DateTime.UtcNow;
+                db.Projects.Add(newProject);
+                db.SaveChanges();
+            }
+            return true;
+        }
+    }
+}
