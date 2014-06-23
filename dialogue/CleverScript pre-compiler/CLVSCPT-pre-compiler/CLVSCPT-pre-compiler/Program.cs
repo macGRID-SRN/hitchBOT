77e9bceeafb93e23883243c7fb3f63a391cc1359
gagichce <gagichce@mcmaster.ca>
2014-06-23 13:12:40 -0400

    initial checkin of parser

---
+++ dialogue/CleverScript pre-compiler/CLVSCPT-pre-compiler/CLVSCPT-pre-compiler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLVSCPT_pre_compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
