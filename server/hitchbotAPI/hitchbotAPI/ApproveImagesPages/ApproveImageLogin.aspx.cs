a70accb90c58b26f6e36e55c01610d3554daacad
gagichce <gagichce@mcmaster.ca>
2014-06-30 17:10:11 -0400

    refactor of page locations

---
+++ server/hitchbotAPI/hitchbotAPI/ApproveImagesPages/ApproveImageLogin.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Data.Entity;
+
+namespace hitchbotAPI
+{
+    public partial class ApproveImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (var db = new Models.Database())
+            {
+                string userName = this.userName.Text;
+                var user = db.Passwords.Include(p => p.hitchBOT).FirstOrDefault(p => p.Username == userName);
+
+                if (user == null)
+                {
+                    lblError.Text = "Username not found!";
+                }
+                else
+                {
+                    if (user.Hash == Models.PasswordHandler.GetHash(passWord.Text))
+                    {
+                        Session["New"] = user;
+                        Response.Redirect("ViewImage.aspx");
+                    }
+                    else
+                    {
+                        lblError.Text = "Incorrect PW";
+                    }
+                }
+            }
+        }
+    }
+}
