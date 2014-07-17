548655b80fe7afe99fe5fab0a834163d9f55b182
gagichce <gagichce@mcmaster.ca>
2014-07-17 18:59:02 -0400

    handled "learning cases"

--- dialogue/CleverScript pre-compiler/CLVSCPT-pre-compiler/CLVSCPT-pre-compiler/Phrase.cs
+++ dialogue/CleverScript pre-compiler/CLVSCPT-pre-compiler/CLVSCPT-pre-compiler/Phrase.cs
@@ -8,16 +8,16 @@ namespace CLVSCPT_pre_compiler
     class Phrase : CleverScriptLine
     {
 
-        public Phrase(string[] text)
-            : base(text)
-        {
-            this.type = "phrase";
-        }
-
         public Phrase(string[] text, string phraseText)
             : base(text)
         {
-            this.text = phraseText;
+            if (phraseText.Contains("->"))
+            {
+                var keep = phraseText.IndexOf("->");
+                this.text = phraseText.Substring(0, keep);
+            }
+            else
+                this.text = phraseText;
             this.type = "phrase";
         }
     }
