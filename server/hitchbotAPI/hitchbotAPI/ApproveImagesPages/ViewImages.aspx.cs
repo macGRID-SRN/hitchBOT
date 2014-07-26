40d5a735e3c2d33915c57e11af3f6ad742786c3a
gagichce <gagichce@mcmaster.ca>
2014-07-26 16:19:10 -0400

    added a collumn to the image viewer, published to live servers

--- server/hitchbotAPI/hitchbotAPI/ApproveImagesPages/ViewImages.aspx.cs
+++ server/hitchbotAPI/hitchbotAPI/ApproveImagesPages/ViewImages.aspx.cs
@@ -20,18 +20,36 @@ namespace hitchbotAPI.ApproveImagesPages
                 using (var db = new Models.Database())
                 {
                     int hitchBOTid = user.hitchBOT.ID;
-                    var imgs = db.Images.Include(i => i.HitchBOT).Where(i => i.HitchBOT.ID == hitchBOTid && (i.TimeApproved == null || i.TimeDenied == null) && DbFunctions.DiffDays( i.TimeTaken, DateTime.UtcNow) <= 1).OrderBy(i => i.TimeTaken).ToList();
-                    foreach (var img in imgs)
+                    var imgs = db.Images.Include(i => i.HitchBOT).Where(i => i.HitchBOT.ID == hitchBOTid && (i.TimeApproved == null || i.TimeDenied == null) && DbFunctions.DiffDays(i.TimeTaken, DateTime.UtcNow) <= 1).OrderBy(i => i.TimeTaken).ToList();
+                    for (int i = 0; i <= imgs.Count; i += 2)
                     {
+                        var img1 = imgs[i];
+                        var img2 = i < imgs.Count -1 ? imgs[i + 1] : null;
+
                         Image newImage = new Image();
-                        newImage.ImageUrl = "http://imgur.com/" + img.url + ".jpg";
+                        newImage.ImageUrl = "http://imgur.com/" + img1.url + ".jpg";
                         newImage.Width = 700;
                         newImage.Style.Add("transform", "rotate(90deg)");
+
                         TableRow tr = new TableRow();
-                        TableCell tc = new TableCell();
-                        tc.Controls.Add(newImage);
-                        tc.Height = newImage.Width;
-                        tr.Cells.Add(tc);
+                        TableCell tc1 = new TableCell();
+
+
+                        tc1.Controls.Add(newImage);
+                        tc1.Height = newImage.Width;
+                        tr.Cells.Add(tc1);
+
+                        if (img2 != null)
+                        {
+                            TableCell tc2 = new TableCell();
+                            Image newImage2 = new Image();
+                            newImage2.ImageUrl = "http://imgur.com/" + img2.url + ".jpg";
+                            newImage2.Width = 700;
+                            newImage2.Style.Add("transform", "rotate(90deg)");
+                            tc2.Controls.Add(newImage2);
+                            tc2.Height = newImage2.Height;
+                            tr.Cells.Add(tc2);
+                        }
                         tableViewImage.Rows.Add(tr);
                     }
                 }
