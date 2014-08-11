c6db353c98d5c3ba8aca530c8ec260e82d4a4630
gagichce <gagichce@mcmaster.ca>
2014-08-11 18:02:47 -0400

    bug fix for the map glitch related to too many locations

--- server/hitchbotAPI/hitchbotAPI/Helpers/LocationHelper.cs
+++ server/hitchbotAPI/hitchbotAPI/Helpers/LocationHelper.cs
@@ -52,11 +52,11 @@ namespace hitchbotAPI.Helpers
         //yes you just found our API key.. wow .. congrats. Too bad it only works on hitchbot.me places.
         public const string gAPIkey = "&key=AIzaSyCEJOxq4a_fUiutFzCukico7aUy--6wMCw";
 
-        public const string gmapsString = "http://maps.googleapis.com/maps/api/staticmap?size=800x800&path=weight:5%7Ccolor:blue%7Cenc:";
+        public const string gmapsString = "http://maps.googleapis.com/maps/api/staticmap?size=800x800&path=weight:4%7Ccolor:blue%7Cenc:";
         public const string gmapsRegionString = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
         public const string gmapsMarkerString = "&markers=icon:http://goo.gl/uwnJCB|size:mid|color:red|label:H|"; 
 
-        private const int maxLocations = 500;
+        private const int maxLocations = 350;
 
         public static string GetEncodedPolyLine(int HitchBotID)
         {
