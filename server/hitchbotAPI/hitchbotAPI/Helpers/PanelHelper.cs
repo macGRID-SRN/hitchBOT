3db4bfc9f041ae0c7611ed1ba96f5d290083e10d
Dominik <domkaukinen@gmail.com>
2014-12-07 19:43:00 -0500

    added LED panel helper

---
+++ server/hitchbotAPI/hitchbotAPI/Helpers/PanelHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hitchbotAPI.Helpers
+{
+    public class PanelHelper
+    {
+
+        double threshhold;
+        Color[][] colorMatrix;
+
+        public PanelHelper(Bitmap bM)
+        {
+            getMatrixFromBitmap(bM);
+        }
+
+        private void getMatrixFromBitmap(Bitmap bM)
+        {
+            int[,] imageRedMap = new int[16, 24];
+
+            int maxRed = 0;
+            int minRed = 255;
+            bM = new Bitmap(bM, 24, 16);
+
+            Color[][] colorMatrix = new Color[24][];
+            for (int i = 0; i < 24; i++)
+            {
+                colorMatrix[i] = new Color[16];
+                for (int j = 0; j < 16; j++)
+                {
+                    int pixel = (bM.GetPixel(i, j).A + bM.GetPixel(i, j).B + bM.GetPixel(i, j).G + bM.GetPixel(i, j).R) / 4;
+                    colorMatrix[i][j] = new Color();
+                    colorMatrix[i][j] = bM.GetPixel(i, j);
+                    if (pixel < minRed)
+                    {
+                        minRed = pixel;
+                    }
+                    if (pixel > maxRed)
+                    {
+                        maxRed = pixel;
+                    }
+                    imageRedMap[j, i] = pixel;
+                }
+            }
+
+            threshhold = (minRed + maxRed) / 2.0;
+
+            this.colorMatrix = colorMatrix;
+            makeBoolArray(imageRedMap);
+        }
+
+        private void makeBoolArray(int[,] imageRedMap)
+        {
+            List<byte> byteList = new List<byte>();
+            List<bool> tempList;
+            BitArray bit;
+            bool[,] boolArray = new bool[16, 24];
+            for (int row = 0; row < imageRedMap.GetLength(0); row++)
+            {
+                tempList = new List<bool>();
+                for (int col = 0; col < imageRedMap.GetLength(1); col++)
+                {
+                    if (imageRedMap[row, col] < threshhold)
+                    {
+                        tempList.Add(false);
+                    }
+                    else
+                    {
+                        tempList.Add(true);
+                    }
+                }
+                bit = new BitArray(tempList.ToArray());
+                byteList.Add(convertToByte(bit));
+            }
+        }
+
+        private byte convertToByte(BitArray bits)
+        {
+            if (bits.Count != 8)
+            {
+                throw new ArgumentException("bits");
+            }
+            byte[] bytes = new byte[1];
+            bits.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+
+
+
+    }
+}
