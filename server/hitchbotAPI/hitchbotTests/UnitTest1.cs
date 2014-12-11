41e60db88155bbbdc18882b1dcd7b2a32d1f136b
gagichce <gagichce@mcmaster.ca>
2014-12-10 21:48:18 -0500

    added failed testing method

--- server/hitchbotAPI/hitchbotTests/UnitTest1.cs
+++ server/hitchbotAPI/hitchbotTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using hitchbotAPI;
 using System.Diagnostics;
+using System.Linq;
 
 namespace hitchbotTests
 {
@@ -17,16 +18,9 @@ namespace hitchbotTests
         [TestMethod]
         public void TestDbIsAvailable()
         {
-            try
-            {
-                var db = new hitchbotAPI.Models.Database();
-                Debug.WriteLine("The Database is mostly probably working.");
-                return;
-            }
-            catch (Exception e)
-            {
-                Assert.Fail("Something went wrong when accessing the DB, either the connection string is wrong, the model has changed or it is currently unavailable. " + e.StackTrace);
-            }
+            //so the last way didn't work.. neither does this.. THIS TEST DOES NOT WORK YET.
+            Assert.IsTrue(hitchbotAPI.Helpers.TestHelper.TestDatabaseAvailable(), "Database is available.");
+            //Assert.Fail("Something went wrong when accessing the DB, either the connection string is wrong, the model has changed or it is currently unavailable.");
         }
     }
 }
