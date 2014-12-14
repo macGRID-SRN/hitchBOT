e2c689876b5b3bac37a21b9cb7cb9cfc97ddd223
gagichce <gagichce@mcmaster.ca>
2014-12-13 19:44:59 -0500

    added method to determine age of azure blob

--- server/hitchbotAPI/hitchbotAPI/Helpers/AzureBlobHelper.cs
+++ server/hitchbotAPI/hitchbotAPI/Helpers/AzureBlobHelper.cs
@@ -31,11 +31,40 @@ namespace hitchbotAPI.Helpers
             {
                 newBlob.UploadFromStream(fileString);
                 newBlob.Properties.ContentType = "text/javascript";
+
+                //we don't want the browser to cache this file because it contains data which is very likely to change frequently.
                 newBlob.Properties.CacheControl = "public, max-age=0";
                 newBlob.SetProperties();
             }
 
             return newBlob.Uri.ToString();
+        }
+
+        private const TimeSpan JS_REBUILD_INTERVAL = new TimeSpan(1, 0, 0);
+
+        /// <summary>
+        /// Determines if the javascript file containing the map data should be rebuilt based on the time passed since it was last built.
+        /// </summary>
+        /// <param name="fileName">the filename of the javascript file that may need to be rebuilt.</param>
+        /// <param name="ID">The ID if the javascript file (also the hitcbotID) to determine if it needs to be rebuilt</param>
+        /// <returns></returns>
+        public static bool ShouldJsBeRebuilt(string fileName, int ID)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imgContainer = blobClient.GetContainerReference(JS_CONTAINER_NAME);
+
+            CloudBlockBlob blob = imgContainer.GetBlockBlobReference(JS_LOCATION_FILE_NAME + ID + JS_FILE_EXTENSION);
+
+            if (blob.Exists())
+            {
+                DateTimeOffset LastModified = blob.Properties.LastModified ?? new DateTime();
+
+                return DateTimeOffset.UtcNow - LastModified > JS_REBUILD_INTERVAL;
+            }
+            return true;
         }
 
         public static void UploadJStoAzure(int hitchbotID)
