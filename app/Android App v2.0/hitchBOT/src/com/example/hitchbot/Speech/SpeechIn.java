c019d1d49d90c0244661f390ffdfb79fe5d49637
Dominik <domkaukinen@gmail.com>
2014-12-21 19:59:44 -0500

    added speech in class (recognizer)

---
+++ app/Android App v2.0/hitchBOT/src/com/example/hitchbot/Speech/SpeechIn.java
@@ -0,0 +1,32 @@
+package com.example.hitchbot.Speech;
+
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+
+public class SpeechIn implements RecognitionListener {
+
+	@Override
+	public void onBeginningOfSpeech() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEndOfSpeech() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPartialResult(Hypothesis arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onResult(Hypothesis arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
