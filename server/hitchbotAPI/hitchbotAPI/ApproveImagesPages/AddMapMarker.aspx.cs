3b38f0ea76ac37acdfbb02881992141496dfff62
gagichce <gagichce@mcmaster.ca>
2015-01-26 12:23:52 -0500

    finished the map marker adding page. should be funcitonal now.

--- server/hitchbotAPI/hitchbotAPI/ApproveImagesPages/AddMapMarker.aspx.cs
+++ server/hitchbotAPI/hitchbotAPI/ApproveImagesPages/AddMapMarker.aspx.cs
@@ -11,7 +11,102 @@ namespace hitchbotAPI.ApproveImagesPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["New"] != null)
+            {
 
+            }
+            else
+            {
+                Response.Redirect("Unauthorized.aspx");
+            }
+        }
+
+        protected void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var popHeaderEnglish = inputHeaderEnglish.Value;
+                var popHeaderGerman = inputHeaderGerman.Value;
+                var popContentEnglish = inputPopupContentEnglish.InnerText;
+                var popContentGerman = inputPopupContentGerman.InnerText;
+
+                var radius = inputRadiusValue.Value;
+                var lat = inputLat.Value;
+                var lng = inputLong.Value;
+
+                double radiusActual;
+                double latActual;
+                double lngActual;
+
+                using (var db = new Models.Database())
+                {
+                    var user = (Models.Password)Session["New"];
+                    var projID = user.Projects.First().ID;
+                    var project = db.Projects.First(l => l.ID == projID);
+
+                    Models.Location location = null;
+
+                    if (!double.TryParse(radius, out radiusActual))
+                    {
+                        setErrorMessage("Selected Radius is not valid!");
+                        return;
+                    }
+
+                    if (!double.TryParse(lat, out latActual))
+                    {
+                        setErrorMessage("Latitude is not valid number!");
+                        return;
+                    }
+
+                    if (!double.TryParse(lng, out lngActual))
+                    {
+                        setErrorMessage("Longitude is not valid number!");
+                        return;
+                    }
+
+                    location = new Models.Location
+                    {
+                        Latitude = latActual,
+                        Longitude = lngActual,
+                        TimeAdded = DateTime.UtcNow,
+                        TakenTime = DateTime.UtcNow
+                    };
+
+                    db.Locations.Add(location);
+                    db.SaveChanges();
+
+                    var wiki = new Models.MapMarker()
+                    {
+                        TargetLocation = location,
+                        RadiusKM = radiusActual,
+                        TimeAdded = DateTime.UtcNow,
+                        Project = project,
+                        Active = true,
+                        HasBeenVisited = false,
+                        BodyText = popContentEnglish,
+                        BodyTextGerman = popContentGerman,
+                        HeaderText = popHeaderEnglish,
+                        HeaderTextGerman = popHeaderGerman
+                    };
+
+                    db.MapMarkers.Add(wiki);
+
+                    db.SaveChanges();
+                }
+
+                Response.Redirect("AddMapMarkerSuccess.aspx");
+            }
+            catch
+            {
+                setErrorMessage("An unknown error occurred. let the sys admin know you saw this message.");
+            }
+        }
+
+        protected void setErrorMessage(string error)
+        {
+            this.errorAlert.Attributes.Remove("class");
+            this.errorAlert.Attributes.Add("class", "alert alert-danger");
+            this.errorAlert.InnerText = error;
         }
     }
 }
---
+++ server/hitchbotAPI/hitchbotAPI/ApproveImagesPages/AddMapMarkerSuccess.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ApproveImagesPages/Shared/PageWithHeader.master" AutoEventWireup="true" CodeBehind="AddMapMarkerSuccess.aspx.cs" Inherits="hitchbotAPI.ApproveImagesPages.AddMapMarkerSuccess" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="StyleContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="mainContent" runat="server">
+    <div class="container">
+        <div class="jumbotron">
+            <h2>Your entry was successfully added!</h2>
+            <div class="row">
+                <div class="col-md-2">
+                    <form action="AddMapMarker.aspx">
+                        <button type="submit" class="btn btn-lg btn-primary">Add Another</button>
+                    </form>
+                </div>
+                <div class="col-md-2">
+                    <form action="ViewMap.aspx">
+                        <button type="submit" class="btn btn-lg">View Map (May not be updated yet)</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="endScripts" runat="server">
+</asp:Content>
