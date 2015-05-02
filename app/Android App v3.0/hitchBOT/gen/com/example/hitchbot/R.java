5a6591f12d551e94c93f4d3d3fb2910d9fa1be32
Dominik <domkaukinen@gmail.com>
2015-05-02 18:42:20 -0400

    added debug activity

--- app/Android App v3.0/hitchBOT/gen/com/example/hitchbot/R.java
+++ app/Android App v3.0/hitchBOT/gen/com/example/hitchbot/R.java
@@ -25,10 +25,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int buttonStart=0x7f080003;
+        public static final int editTextName=0x7f080002;
+        public static final int editTextSpecInfo=0x7f080004;
+        public static final int textViewName=0x7f080000;
+        public static final int textViewSpecInfo=0x7f080001;
     }
     public static final class layout {
         public static final int activity_hitch=0x7f030000;
+        public static final int activity_speech=0x7f030001;
     }
     public static final class menu {
         public static final int hitch=0x7f070000;
@@ -37,6 +43,7 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int missing_info=0x7f050003;
     }
     public static final class style {
         /** 
