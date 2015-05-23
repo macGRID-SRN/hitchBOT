7d7ba5d7c1c0f356aefba9acd23b92d771868c58
gagichce <gagichce@mcmaster.ca>
2015-05-23 15:32:26 -0400

    enabled migrations and set up initial server

---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Dal/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hitchbot_secure_api.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace hitchbot_secure_api.Dal
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("Database")
+        {
+            
+        }
+
+        //Dbsets go here.
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace hitchbot_secure_api.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<hitchbot_secure_api.Dal.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(hitchbot_secure_api.Dal.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
