c81f68b96295de8504d788cd0fc2ef0e0256d8e6
gagichce <gagichce@mcmaster.ca>
2015-05-23 15:06:58 -0400

    created new hitchbot-project

---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace hitchbot_secure_api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(hitchbot_secure_api.Startup))]
+
+namespace hitchbot_secure_api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
