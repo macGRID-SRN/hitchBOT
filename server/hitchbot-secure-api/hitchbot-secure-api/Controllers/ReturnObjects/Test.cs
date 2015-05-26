c2c8c6f8df603c3d665d2c9c4e3c9a5f0d1af206
gagichce <gagichce@mcmaster.ca>
2015-05-26 16:05:49 -0400

    added controller for testing + fixed model constructor

---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Controllers/ReturnObjects/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hitchbot_secure_api.Controllers.ReturnObjects;
+using Newtonsoft.Json;
+
+namespace hitchbot_secure_api.Controllers
+{
+    public partial class SystemController
+    {
+        public class ReturnTest : GenericHitchBot
+        {
+            public string Dec { get; set; }
+
+            public int? _Dec
+            {
+
+                get
+                {
+                    if (string.IsNullOrWhiteSpace(Dec))
+                        return null;
+                    int temp;
+                    if (int.TryParse(Dec, out temp))
+                        return temp;
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Controllers/SystemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace hitchbot_secure_api.Controllers
+{
+    public partial class SystemController : ApiController
+    {
+        public async Task<IHttpActionResult> TestModel([FromBody] ReturnTest Context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Model state is not valid");
+
+
+            return Ok();
+        }
+
+        public async Task<IHttpActionResult> TestDatabase()
+        {
+            using (var db = new Dal.DatabaseContext())
+            {
+                return Ok(db.Database.Exists());
+            }
+        }
+    }
+}
