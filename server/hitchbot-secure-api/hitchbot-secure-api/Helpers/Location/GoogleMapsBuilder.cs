43e876aa6dbe4840b3458ffc69e2b428d4b729e2
gagichce <gagichce@mcmaster.ca>
2015-06-09 12:12:56 -0400

    map now centers around the latest location - static zoom level to allow the country label to be still

--- server/hitchbot-secure-api/hitchbot-secure-api/Helpers/Location/GoogleMapsBuilder.cs
+++ server/hitchbot-secure-api/hitchbot-secure-api/Helpers/Location/GoogleMapsBuilder.cs
@@ -166,7 +166,7 @@ function AutoCenter(e){
 function initialize(){
 
 var e={
-	center:centerPoint,
+	center:currentPoint,
 	zoom:centerZoom
 };
 var t=new google.maps.Map(document.getElementById(""map-canvas""),e);
@@ -180,8 +180,8 @@ if(autocenter){AutoCenter(t)}
 
 var centerPoint=new google.maps.LatLng(48.1384,11.573399999999992);
 
-var centerZoom=10;
-var autocenter=true;
+var centerZoom=7;
+var autocenter=false;
 google.maps.event.addDomListener(window,""load"",initialize);";
 
             return stringy;
