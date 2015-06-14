e45f7754ed36e7e7aa16df3a29ad34af5200ba42
gagichce <gagichce@mcmaster.ca>
2015-06-13 20:41:46 -0400

    added bcrypt password verification

---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Helpers/AccountHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleCrypto;
+
+namespace hitchbot_secure_api.Helpers
+{
+    public static class AccountHelper
+    {
+        public static bool VerifyPassword(string password, string salt, string hash)
+        {
+            ICryptoService service = new PBKDF2();
+            string hashed = service.Compute(password, salt);
+
+            return service.Compare(hash, hashed);
+        }
+
+        public static SaltHashPair GetNewHashAndSalt(string password)
+        {
+            ICryptoService service = new PBKDF2();
+
+            var salt = service.GenerateSalt();
+
+            var hash = service.Compute(password);
+
+            return new SaltHashPair
+            {
+                Salt = salt,
+                Hash = hash
+            };
+        }
+
+        public class SaltHashPair
+        {
+            public string Salt { get; set; }
+            public string Hash { get; set; }
+        }
+    }
+}
