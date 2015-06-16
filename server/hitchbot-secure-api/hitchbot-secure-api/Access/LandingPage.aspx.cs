826e24f0bb0ee49db6e5d238b70c4b7c13b81426
gagichce <gagichce@mcmaster.ca>
2015-06-16 11:04:59 -0400

    added the new pages minus jquery apparently

---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Access/LandingPage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hitchbot_secure_api.Access
+{
+    public partial class LandingPage : System.Web.UI.Page
+    {
+        private int hitchBotId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["New"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Redirect("Unauthorized.aspx");
+            }
+        }
+    }
+}
---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Access/Unauthorized.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Unauthorized.aspx.cs" Inherits="hitchbot_secure_api.Access.Unauthorized" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="font-size: x-large">
+    <form id="form1" runat="server">
+    <div>You are not authorized to view this page.</div>
+        <asp:HyperLink ID="HyperLink1" runat="server" href="Login.aspx">Sign In</asp:HyperLink>
+    </form>
+</body>
+</html>
