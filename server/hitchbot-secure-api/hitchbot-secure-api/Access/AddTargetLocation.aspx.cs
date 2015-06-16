3ac1991b8a79a5f8890cef287cfe2aedb34240a0
gagichce <gagichce@mcmaster.ca>
2015-06-16 11:04:59 -0400

    added the new pages minus jquery apparently

---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Access/AddTargetLocation.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hitchbot_secure_api.Access
+{
+    public partial class AddTargetLocation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["New"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Redirect("Unauthorized.aspx");
+            }
+        }
+
+        protected void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var wikiEntry = inputWiki1.InnerText;
+                var radius = inputRadiusValue.Value;
+                var name = inputName.Value;
+                var lat = inputLat.Value;
+                var lng = inputLong.Value;
+
+                double? radiusActual = null;
+                double latActual;
+                double lngActual;
+
+                using (var db = new Dal.DatabaseContext())
+                {
+                    var user = (Models.LoginAccount)Session["New"];
+                    var hitchbotId = user.HitchBotId;
+
+                    Models.Location location = null;
+
+                    if (LocationCheckBox.Checked)
+                    {
+                        /*
+                        nullable double parse code borrowed from
+                        http://stackoverflow.com/questions/3390750/how-to-use-int-tryparse-with-nullable-int
+                        */
+                        double tmp;
+
+                        if (!double.TryParse(radius, out tmp))
+                        {
+                            setErrorMessage("Selected Radius is not valid!");
+                            return;
+                        }
+                        radiusActual = tmp;
+
+                        if (!double.TryParse(lat, out latActual))
+                        {
+                            setErrorMessage("Latitude is not valid number!");
+                            return;
+                        }
+
+                        if (!double.TryParse(lng, out lngActual))
+                        {
+                            setErrorMessage("Longitude is not valid number!");
+                            return;
+                        }
+
+                        location = new Models.Location
+                        {
+                            Latitude = latActual,
+                            Longitude = lngActual,
+                            TimeAdded = DateTime.UtcNow,
+                            TakenTime = DateTime.UtcNow
+                        };
+
+                        db.Locations.Add(location);
+                        db.SaveChanges();
+
+                    }
+
+                    var wiki = new Models.CleverscriptContent
+                    {
+                        LocationId = location.Id,
+                        CleverText = wikiEntry,
+                        EntryName = name,
+                        RadiusKm = radiusActual,
+                        HitchBotId = hitchbotId,
+                        TimeAdded = DateTime.UtcNow
+                    };
+
+                    db.CleverscriptContents.Add(wiki);
+
+                    db.SaveChanges();
+                }
+
+                Response.Redirect("AddTargetSuccess.aspx");
+            }
+            catch
+            {
+                setErrorMessage("An unknown error occurred. let the sys admin know you saw this message.");
+            }
+        }
+
+        protected void setErrorMessage(string error)
+        {
+            this.errorAlert.Attributes.Remove("class");
+            this.errorAlert.Attributes.Add("class", "alert alert-danger");
+            this.errorAlert.InnerText = error;
+        }
+    }
+}
---
+++ server/hitchbot-secure-api/hitchbot-secure-api/Access/LandingPage.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Access/Shared/PageWithHeader.master" AutoEventWireup="true" CodeBehind="LandingPage.aspx.cs" Inherits="hitchbot_secure_api.Access.LandingPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="StyleContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="mainContent" runat="server">
+       <div class="container">
+        <div class="jumbotron">
+            <h1>Hi there!</h1>
+            <p>
+                Welcome to the new API! You just might like it here.
+            </p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h3>What's new?</h3></div>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <h4 class="list-grou-item-heading">Geo-relevant Cleverscript Entries!
+                    </h4>
+                    <p class="list-group-item-text">
+                        Ready for use. You can use this tool to enter Cleverscript sentences and info that will be read by hitchBOT itself!
+                    </p>
+                </li>
+                <li class="list-group-item">
+                    <h4 class="list-grou-item-heading">Website Styling!
+                    </h4>
+                    <p class="list-group-item-text">
+                        V extra pretty.
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="endScripts" runat="server">
+</asp:Content>
